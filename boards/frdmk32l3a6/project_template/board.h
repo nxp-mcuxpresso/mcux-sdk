eb8816bf84550e6eaf43fecbb7fbef6283be5951
microDevSys <76966490+microDevSys@users.noreply.github.com>
2021-02-19 10:54:15 +0100

    [frdmk32l3a6] Update board.h and pin_mux template files
    * Update pin_mux.c
    
    add on line 96, the function to allow access to GPIOE base address, so the mcu don't go in hardfault for initialisation.
    
    * Update pin_mux.c
    
    add alsp in line 420
    
    * Update board.h
    
    add definition to LED connected to PORTE0
    
    * Update pin_mux.h
    
    add definition for red led on board
    
    * Update pin_mux.c
    
    add initialisation of other red led in the board
    
    Signed-off-by: EmergReanimator <emerg.reanimator@gmail.com>

--- boards/frdmk32l3a6/project_template/board.h
+++ boards/frdmk32l3a6/project_template/board.h
@@ -95,6 +95,13 @@
 #define BOARD_LED4_GPIO_PIN 0U
 #endif
 
+#ifndef BOARD_LED4_GPIO
+#define BOARD_LED4_GPIO GPIOE
+#endif
+#ifndef BOARD_LED4_GPIO_PIN
+#define BOARD_LED4_GPIO_PIN 0U
+#endif
+
 #define LED1_INIT(output)                                        \
     GPIO_PinWrite(BOARD_LED1_GPIO, BOARD_LED1_GPIO_PIN, output); \
     BOARD_LED1_GPIO->PDDR |= (1U << BOARD_LED1_GPIO_PIN)                          /*!< Enable target LED1 */
@@ -115,6 +122,14 @@
 #define LED3_ON()           GPIO_PortSet(BOARD_LED3_GPIO, 1U << BOARD_LED3_GPIO_PIN)    /*!< Turn on target LED3 */
 #define LED3_OFF()          GPIO_PortClear(BOARD_LED3_GPIO, 1U << BOARD_LED3_GPIO_PIN)  /*!< Turn off target LED3 */
 #define LED3_TOGGLE()       GPIO_PortToggle(BOARD_LED3_GPIO, 1U << BOARD_LED3_GPIO_PIN) /*!< Toggle on target LED3 */
+
+#define LED4_INIT(output)                                        \
+    GPIO_PinWrite(BOARD_LED4_GPIO, BOARD_LED4_GPIO_PIN, output); \
+    BOARD_LED4_GPIO->PDDR |= (1U << BOARD_LED4_GPIO_PIN)
+#define LED4_ON()           GPIO_PortSet(BOARD_LED4_GPIO, 1U << BOARD_LED4_GPIO_PIN)    /*!< Turn on target LED */
+#define LED4_OFF()          GPIO_PortClear(BOARD_LED4_GPIO, 1U << BOARD_LED4_GPIO_PIN)  /*!< Turn off target LED */
+#define LED4_TOGGLE()       GPIO_PortToggle(BOARD_LED4_GPIO, 1U << BOARD_LED4_GPIO_PIN) /*!< Toggle on target LED */
+
 #define LLWU_SW_GPIO        BOARD_SW2_GPIO
 #define LLWU_SW_PORT        BOARD_SW2_PORT
 #define LLWU_SW_GPIO_PIN    BOARD_SW2_GPIO_PIN
