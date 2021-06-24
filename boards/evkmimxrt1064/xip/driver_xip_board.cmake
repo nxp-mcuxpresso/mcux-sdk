e1cca9e33a6c9c0983d5b582f001fcdacd878529
Dane Walton <dane.walton@microsoft.com>
2021-06-24 14:13:39 -0700

    make target includes public for lib scenario

--- boards/evkmimxrt1064/xip/driver_xip_board.cmake
+++ boards/evkmimxrt1064/xip/driver_xip_board.cmake
@@ -6,7 +6,7 @@ target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/evkmimxrt1064_flexspi_nor_config.c
 )
 
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PRIVATE
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
     ${CMAKE_CURRENT_LIST_DIR}/.
 )
 
--- drivers/lpuart/driver_lpuart.cmake
+++ drivers/lpuart/driver_lpuart.cmake
@@ -6,7 +6,7 @@ target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/fsl_lpuart.c
 )
 
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PRIVATE
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
     ${CMAKE_CURRENT_LIST_DIR}/.
 )
 
