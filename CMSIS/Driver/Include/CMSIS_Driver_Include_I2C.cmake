817b6afd72e79f7adc0a0c432ede276624a778d1
Dane Walton <dane.walton@microsoft.com>
2021-06-25 12:19:23 -0700

    make cmsis target includes public
    Signed-off-by: Dane Walton <dane.walton@microsoft.com>
    Signed-off-by: EmergReanimator <emerg.reanimator@gmail.com>

--- CMSIS/Driver/Include/CMSIS_Driver_Include_I2C.cmake
+++ CMSIS/Driver/Include/CMSIS_Driver_Include_I2C.cmake
@@ -5,7 +5,7 @@ message("CMSIS_Driver_Include_I2C component is included.")
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
 )
 
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PRIVATE
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
     ${CMAKE_CURRENT_LIST_DIR}/.
 )
 
--- CMSIS/Include/CMSIS_Include_core_cm4.cmake
+++ CMSIS/Include/CMSIS_Include_core_cm4.cmake
@@ -5,7 +5,7 @@ message("CMSIS_Include_core_cm4 component is included.")
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
 )
 
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PRIVATE
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
     ${CMAKE_CURRENT_LIST_DIR}/.
 )
 
