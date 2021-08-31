c5ac7e5b69754ac1bb7975751607095c8bc5f0d2
Susan Su <susan.su@nxp.com>
2021-08-31 21:03:05 +0800

    zephyr: Fill the device and driver support gap in mcux-sdk
     - Add missing device support for K8x, KE18F, KL25Z, KW2x, KW3x and
       KW4x. Reuse files in hal_nxp.
     - Add lpsci driver which is needed for KL25Z.
    
    Signed-off-by: Susan Su <susan.su@nxp.com>

---
+++ devices/MKW41Z4/device_CMSIS.cmake
@@ -0,0 +1,13 @@
+#Description: device_CMSIS; user_visible: False
+include_guard(GLOBAL)
+message("device_CMSIS component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)
+
+
+include(CMSIS_Include_core_cm)
