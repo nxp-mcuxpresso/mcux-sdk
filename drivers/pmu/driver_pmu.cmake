7057bc4d25302b2bfd23a508a804884f21c1ab29
Daniel DeGrasse <daniel.degrasse@nxp.com>
2021-12-13 14:48:03 -0600

    Add pmu driver cmake file
    Adds pmu driver cmake file, in preparation for including this driver
    component into Zephyr.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

---
+++ drivers/pmu/driver_pmu.cmake
@@ -0,0 +1,14 @@
+#Description: PMU Driver; user_visible: True
+include_guard(GLOBAL)
+message("driver_pmu component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/fsl_pmu.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)
+
+
+include(driver_common)
