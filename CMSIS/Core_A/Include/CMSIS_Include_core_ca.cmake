a973771ef7b02c8b25055078c57c3a1ee362254c
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2022-01-28 18:02:42 +0800

    CMSIS: Core_A: Add cache maintain support for ARMv8-A
    Add I-Cache and D-Cache maintain operations for armv8a.
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- CMSIS/Core_A/Include/CMSIS_Include_core_ca.cmake
+++ CMSIS/Core_A/Include/CMSIS_Include_core_ca.cmake
@@ -3,6 +3,7 @@ include_guard(GLOBAL)
 message("CMSIS_Include_core_ca component is included.")
 
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/../Source/cache_armv8a.c
     ${CMAKE_CURRENT_LIST_DIR}/../Source/mmu_armv8a.c
 )
 
