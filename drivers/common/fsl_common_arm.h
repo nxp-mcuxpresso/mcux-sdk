e4c9a177823fe54a63bab6380973f15c326ae24d
Stephane Viau <stephane.viau@nxp.com>
2022-01-31 13:52:50 +0000

    fsl_common_arm: Update CS macro name
    CS is used throughout several drivers; let's rename this macro to avoid
    conflicts.
    
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

--- drivers/common/fsl_common_arm.h
+++ drivers/common/fsl_common_arm.h
@@ -396,9 +396,9 @@ _Pragma("diag_suppress=Pm120")
 
 #elif (defined(__GNUC__))
 #if defined(__ARM_ARCH_8A__) /* This macro is ARMv8-A specific */
-#define CS "//"
+#define __CS "//"
 #else
-#define CS "@"
+#define __CS "@"
 #endif
 
 /* For GCC, when the non-cacheable section is required, please define "__STARTUP_INITIALIZE_NONCACHEDATA"
@@ -407,9 +407,9 @@ _Pragma("diag_suppress=Pm120")
 #define AT_NONCACHEABLE_SECTION_INIT(var) __attribute__((section("NonCacheable.init"))) var
 #define AT_NONCACHEABLE_SECTION_ALIGN_INIT(var, alignbytes) \
     __attribute__((section("NonCacheable.init"))) var __attribute__((aligned(alignbytes)))
-#define AT_NONCACHEABLE_SECTION(var) __attribute__((section("NonCacheable,\"aw\",%nobits " CS))) var
+#define AT_NONCACHEABLE_SECTION(var) __attribute__((section("NonCacheable,\"aw\",%nobits " __CS))) var
 #define AT_NONCACHEABLE_SECTION_ALIGN(var, alignbytes) \
-    __attribute__((section("NonCacheable,\"aw\",%nobits " CS))) var __attribute__((aligned(alignbytes)))
+    __attribute__((section("NonCacheable,\"aw\",%nobits " __CS))) var __attribute__((aligned(alignbytes)))
 #else
 #error Toolchain not supported.
 #endif
