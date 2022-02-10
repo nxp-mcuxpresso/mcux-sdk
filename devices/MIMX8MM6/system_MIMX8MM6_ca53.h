893cff89b75225520a8fd21b99af87fcd21e117b
Stephane Viau <stephane.viau@nxp.com>
2022-02-10 16:05:41 +0000

    MIMX8M{L8,M6,N6}_ca53: Define default System clock value
    Note that this value should be updated at runtime with the real clock value.
    
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

--- devices/MIMX8MM6/system_MIMX8MM6_ca53.h
+++ devices/MIMX8MM6/system_MIMX8MM6_ca53.h
@@ -58,6 +58,8 @@ extern "C" {
 
 #include <stdint.h>
 
+/* Define clock source values */
+#define DEFAULT_SYSTEM_CLOCK   8000000U /* Default System clock value */
 
 /**
  * @brief System clock frequency (core clock)
