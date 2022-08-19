e9a3f29f9a522df786d4f68d97e6bddb6c273101
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2022-08-19 16:34:42 +0800

    boards: Add an new directory 'evkmimx8mm_ca53' for Cortex-A53
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

---
+++ boards/evkmimx8mm_ca53/board.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019-2022 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#if __has_include("app_board.h")
+#include "app_board.h"
+#endif
+#include "fsl_clock.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/* TODO Implement CLOCK_GetRootClockFreq() */
+#define kCLOCK_Root_Uart4                 4
+#define CLOCK_GetRootClockFreq(clockRoot) OSC24M_CLK_FREQ
+
+/*! @brief The board name */
+#define BOARD_NAME        "MIMX8MM-EVK"
+#define MANUFACTURER_NAME "NXP"
+#define BOARD_DOMAIN_ID   (1U)
+/* The UART to use for debug messages. */
+#define BOARD_DEBUG_UART_TYPE     kSerialPort_Uart
+#define BOARD_DEBUG_UART_BAUDRATE (115200U)
+#define BOARD_DEBUG_UART_BASEADDR UART4_BASE
+#define BOARD_DEBUG_UART_INSTANCE (4U)
+#define BOARD_DEBUG_UART_CLK_FREQ (CLOCK_GetRootClockFreq(kCLOCK_Root_Uart4))
+#define BOARD_UART_IRQ         UART4_IRQn
+#define BOARD_UART_IRQ_HANDLER UART4_IRQHandler
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* __cplusplus */
+
+/*******************************************************************************
+ * API
+ ******************************************************************************/
+
+void BOARD_InitDebugConsole(void);
+void BOARD_InitMemory(void);
+void BOARD_RdcInit(void);
+
+#if defined(__cplusplus)
+}
+#endif /* __cplusplus */
+
+#endif /* _BOARD_H_ */
---
+++ boards/evkmimx8mm_ca53/memory.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2021-2022 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _MEMORY_H_
+#define _MEMORY_H_
+
+/* Virtual address space */
+#define DDR_BASE		0x93c00000
+
+#endif /* _MEMORY_H_ */
