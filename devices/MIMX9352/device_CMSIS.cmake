554351af5ca0a94aa0fc1a25c4e88b5ee405eaf2
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-11-01 14:56:20 +0800

    imx93: add device camke files for CMSIS and driver_reset
    Add cmake file: device_CMSIS.cmake and driver_reset.cmake.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

---
+++ devices/MIMX9352/device_CMSIS.cmake
@@ -0,0 +1,19 @@
+#Description: device_CMSIS; user_visible: False
+include_guard(GLOBAL)
+message("device_CMSIS component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)
+
+
+if(${MCUX_DEVICE} STREQUAL "MIMX9352")
+    include(CMSIS_Include_core_cm)
+endif()
+
+if(${MCUX_DEVICE} STREQUAL "MIMX9352_ca55")
+    include(CMSIS_Include_core_ca)
+endif()
