2d8291953670adfe90f147e74c5e5e72a27edb2f
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-11-01 11:00:53 +0800

    imx93: update fsl_device_registers.h to add Cortex-A55 support
    Use the same header file both for Cortex-A Core and Cortex-M Core.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- devices/MIMX9352/fsl_device_registers.h
+++ devices/MIMX9352/fsl_device_registers.h
@@ -15,7 +15,16 @@
  *
  * The CPU macro should be declared in the project or makefile.
  */
-#if (defined(CPU_MIMX9352CVUXK_cm33) || defined(CPU_MIMX9352DVUXM_cm33))
+#if (defined(CPU_MIMX9352AVTXM_ca55) || defined(CPU_MIMX9352CVUXK_ca55) || defined(CPU_MIMX9352DVUXM_ca55))
+
+#define MIMX9352_ca55_SERIES
+
+/* CMSIS-style register definitions */
+#include "MIMX9352_ca55.h"
+/* CPU specific feature definitions */
+#include "MIMX9352_ca55_features.h"
+
+#elif (defined(CPU_MIMX9352CVUXK_cm33) || defined(CPU_MIMX9352DVUXM_cm33))
 
 #define MIMX9352_cm33_SERIES
 
