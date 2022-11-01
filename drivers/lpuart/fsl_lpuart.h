27c9b071f85587a2ad4c2067ae110d94c728292f
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-11-01 16:50:37 +0800

    lpuart: enable driver aarch64 compatible
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- drivers/lpuart/fsl_lpuart.h
+++ drivers/lpuart/fsl_lpuart.h
@@ -650,9 +650,9 @@ uint32_t LPUART_GetEnabledInterrupts(LPUART_Type *base);
  * @param base LPUART peripheral base address.
  * @return LPUART data register addresses which are used both by the transmitter and receiver.
  */
-static inline uint32_t LPUART_GetDataRegisterAddress(LPUART_Type *base)
+static inline uintptr_t LPUART_GetDataRegisterAddress(LPUART_Type *base)
 {
-    return (uint32_t) & (base->DATA);
+    return (uintptr_t) & (base->DATA);
 }
 
 /*!
