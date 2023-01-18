208fe22a0e3c1120011a84cbebc6240a35cf2ac4
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2023-01-19 00:08:43 +0800

    all devices cmake: add MIMX9352 support
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- all_devices.cmake
+++ all_devices.cmake
@@ -2240,3 +2240,21 @@ if(${MCUX_DEVICE} STREQUAL "MK22F12")
     include(all_lib_device_MK22F12)
 
 endif()
+
+if(${MCUX_DEVICE} STREQUAL "MIMX9352")
+    list(APPEND CMAKE_MODULE_PATH
+        ${CMAKE_CURRENT_LIST_DIR}/devices/MIMX9352
+    )
+
+    include(all_lib_device_MIMX9352)
+
+endif()
+
+if(${MCUX_DEVICE} STREQUAL "MIMX9352_ca55")
+    list(APPEND CMAKE_MODULE_PATH
+        ${CMAKE_CURRENT_LIST_DIR}/devices/MIMX9352
+    )
+
+    include(all_lib_device_MIMX9352_ca55)
+
+endif()
