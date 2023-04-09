a420127b03a3094f413d822c045826d14390979a
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2023-04-09 16:11:19 +0800

    driver: cache: add xcache driver support
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

---
+++ drivers/cache/xcache/driver_cache_xcache.cmake
@@ -0,0 +1,15 @@
+#Description: XCACHE Driver; user_visible: True
+include_guard(GLOBAL)
+message("driver_cache_xcache component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/fsl_cache.c
+)
+
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)
+
+
+include(driver_common)
