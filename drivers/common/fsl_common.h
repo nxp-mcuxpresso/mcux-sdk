iuliana-prodan/hal_nxp

fsl_common: add core specific common file
Update driver_common to include only needed files
and/or drivers based on the core - ARM or DSP.
For NXP Audio DSP include fsl_common_dsp.h and do
not add device_CMSIS.

Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

mcux/mcux-sdk/drivers/common/fsl_common.h
@@ -324,7 +324,7 @@ void SDK_DelayAtLeastUs(uint32_t delayTime_us, uint32_t coreClock_Hz);
 
 #if (defined(__DSC__) && defined(__CW__))
 #include "fsl_common_dsc.h"
-#elif defined(__XCC__)
+#elif defined(__XCC__) || defined(CONFIG_SOC_FAMILY_NXP_ADSP)
 #include "fsl_common_dsp.h"
 #else
 #include "fsl_common_arm.h"

nxp-mcuxpresso/mcux-sdk

drivers/common/fsl_common.h
@@ -322,7 +322,7 @@ void SDK_DelayAtLeastUs(uint32_t delayTime_us, uint32_t coreClock_Hz);
 
 #if (defined(__DSC__) && defined(__CW__))
 #include "fsl_common_dsc.h"
-#elif defined(__XCC__)
+#elif defined(__XCC__) || defined(__XTENSA__)
 #include "fsl_common_dsp.h"
 #else
 #include "fsl_common_arm.h"