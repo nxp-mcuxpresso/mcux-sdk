e42cae9825997b3886129527df5fef66373aab02
Iuliana Prodan <iuliana.prodan@nxp.com>
2023-06-23 12:28:22 +0300

    mcux-sdk: dsp: add support for MU
    Include MU driver for i.MX8M DSP.
    
    Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

--- devices/MIMX8ML8/all_lib_device_MIMX8ML8_dsp.cmake
+++ devices/MIMX8ML8/all_lib_device_MIMX8ML8_dsp.cmake
@@ -3,6 +3,7 @@ list(APPEND CMAKE_MODULE_PATH
     ${CMAKE_CURRENT_LIST_DIR}/../../components/uart
     ${CMAKE_CURRENT_LIST_DIR}/../../drivers/common
     ${CMAKE_CURRENT_LIST_DIR}/../../drivers/iuart
+    ${CMAKE_CURRENT_LIST_DIR}/../../drivers/mu
     ${CMAKE_CURRENT_LIST_DIR}/drivers
 )
 
@@ -10,3 +11,4 @@ list(APPEND CMAKE_MODULE_PATH
 # Copy the cmake components into projects
 #    include(driver_iuart)
 #    include(driver_common)
+#    include(driver_mu)
