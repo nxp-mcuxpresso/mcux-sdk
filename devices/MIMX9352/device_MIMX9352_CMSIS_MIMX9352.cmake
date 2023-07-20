b11eb36263ed7465fca7f375723e87b4a65a8226
Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>
2023-07-20 13:33:13 +0300

    devices: add MIMX9352 support
    Ported from mcu-sdk, commit id of submoule/devices is:
    515aafc73c8650fc9b6ea2809ed754963ce63c21
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>
    Signed-off-by: Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>

---
+++ devices/MIMX9352/device_MIMX9352_CMSIS_MIMX9352.cmake
@@ -0,0 +1,15 @@
+include_guard()
+message("device_MIMX9352_CMSIS component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/system_MIMX9352_cm33.c
+)
+
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)
+
+
+include(CMSIS_Include_core_cm_MIMX9352)
+
