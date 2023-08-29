c2cbd0fed2ddce28abf8bb5739b8275e72ca5c38
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2023-08-29 22:31:42 +0100

    drivers: flexcomm: Delete old driver files that have moved
    The flexcomm driver layout has changed. Delete older driver
    files and update cmake files to point to the new driver
    
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- drivers/flexcomm/driver_flexcomm_spi.cmake
+++ drivers/flexcomm/driver_flexcomm_spi.cmake
@@ -3,11 +3,11 @@ include_guard(GLOBAL)
 message("driver_flexcomm_spi component is included.")
 
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
-    ${CMAKE_CURRENT_LIST_DIR}/fsl_spi.c
+    ${CMAKE_CURRENT_LIST_DIR}/spi/fsl_spi.c
 )
 
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-    ${CMAKE_CURRENT_LIST_DIR}/.
+    ${CMAKE_CURRENT_LIST_DIR}/spi/.
 )
 
 
--- drivers/flexcomm/driver_flexcomm_usart.cmake
+++ drivers/flexcomm/driver_flexcomm_usart.cmake
@@ -3,11 +3,11 @@ include_guard(GLOBAL)
 message("driver_flexcomm_usart component is included.")
 
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
-    ${CMAKE_CURRENT_LIST_DIR}/fsl_usart.c
+    ${CMAKE_CURRENT_LIST_DIR}/usart/fsl_usart.c
 )
 
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-    ${CMAKE_CURRENT_LIST_DIR}/.
+    ${CMAKE_CURRENT_LIST_DIR}/usart/.
 )
 
 
