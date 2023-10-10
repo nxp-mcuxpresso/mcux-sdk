bb8b34de85998f5f697f08472fc4655ab6a9f69e
Yangbo Lu <yangbo.lu@nxp.com>
2023-10-10 15:13:13 +1100

    devices: MIMX9352: initialize g_clockSourceFreq with default values
    Initialize g_clockSourceFreq with default values configured by ROM.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- devices/MIMX9352/drivers/fsl_clock.c
+++ devices/MIMX9352/drivers/fsl_clock.c
@@ -16,7 +16,26 @@
 
 static CCM_Type *ccm_base = CCM_CTRL;
 
-volatile uint32_t g_clockSourceFreq[kCLOCK_Ext + 1];
+/* ROM initializes PLLs with default frequencies except audio/video/ext */
+volatile uint32_t g_clockSourceFreq[kCLOCK_Ext + 1] = {
+    24000000U,   /* kCLOCK_Osc24M          */
+    2000000000U, /* kCLOCK_ArmPll          */
+    2000000000U, /* kCLOCK_ArmPllOut       */
+    1000000000U, /* kCLOCK_DramPll         */
+    1000000000U, /* kCLOCK_DramPllOut      */
+    4000000000U, /* kCLOCK_SysPll1         */
+    1000000000U, /* kCLOCK_SysPll1Pfd0     */
+    500000000U,  /* kCLOCK_SysPll1Pfd0Div2 */
+    800000000U,  /* kCLOCK_SysPll1Pfd1     */
+    400000000U,  /* kCLOCK_SysPll1Pfd1Div2 */
+    625000000U,  /* kCLOCK_SysPll1Pfd2     */
+    312500000U,  /* kCLOCK_SysPll1Pfd2Div2 */
+    0U,          /* kCLOCK_AudioPll1       */
+    0U,          /* kCLOCK_AudioPll1Out    */
+    0U,          /* kCLOCK_VideoPll1       */
+    0U,          /* kCLOCK_VideoPll1Out    */
+    0U           /* kCLOCK_Ext             */
+};
 
 uint32_t CLOCK_GetIpFreq(clock_root_t name)
 {
