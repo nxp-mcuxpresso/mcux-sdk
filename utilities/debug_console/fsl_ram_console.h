55a5e52e2eae9a8b654cb43ef2255c88b645d756
Jiafei Pan <Jiafei.Pan@nxp.com>
2023-10-10 17:16:16 +0800

    ram_console: record console information in header room
    Add RAM Console header room, the flag string is used to check
    whether RAM Console memory space is valid, and also record
    RAM Console's buffer address, size and current cursor position
    in the header information.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

---
+++ utilities/debug_console/fsl_ram_console.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __FSL_RAM_CONSOLE_H__
+#define __FSL_RAM_CONSOLE_H__
+
+#define RAM_CONSOLE_HEAD_STR "RAM_CONSOLE"
+
+#define RAM_CONSOLE_HEAD_SIZE 64
+
+struct ram_console_header {
+    char flag_string[12];
+    char *ram_console_buf_addr;
+    size_t ram_console_buf_size;
+    size_t pos;
+};
+
+#endif /* __FSL_RAM_CONSOLE_H__ */
