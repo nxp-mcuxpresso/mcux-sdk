5a68b2f71adb42f8fa0cc63de804dd19bd48ee31
Jiafei Pan <Jiafei.Pan@nxp.com>
2023-12-13 17:37:50 +0800

    MIMX9352: use the latest version of debug_console driver
    The debug_console driver in utilities/debug_console/debug_console/
    is deprecated, let use the latest version of driver in directory
    utilities/debug_console/
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- devices/MIMX9352/all_lib_device_MIMX9352_ca55.cmake
+++ devices/MIMX9352/all_lib_device_MIMX9352_ca55.cmake
@@ -22,7 +22,7 @@ list(APPEND CMAKE_MODULE_PATH
     ${CMAKE_CURRENT_LIST_DIR}/../../drivers/rgpio
     ${CMAKE_CURRENT_LIST_DIR}/../../drivers/sai
     ${CMAKE_CURRENT_LIST_DIR}/../../utilities/assert
-    ${CMAKE_CURRENT_LIST_DIR}/../../utilities/debug_console
+    ${CMAKE_CURRENT_LIST_DIR}/../../utilities
     ${CMAKE_CURRENT_LIST_DIR}/../../utilities/misc_utilities
     ${CMAKE_CURRENT_LIST_DIR}/drivers
 )
