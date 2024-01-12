2f2d58c323b9e72f7ff569a4d1cc2db11753b17f
Jiafei Pan <Jiafei.Pan@nxp.com>
2024-01-12 17:58:22 +0800

    misc_utilities: use stub syscall for imx8mm/n/p, imx93 and cortex-a core
    It is used to fix build errors for some syscall API (such as _clock _open ..) are not
    defined when using toolchain 12.2.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- utilities/misc_utilities/utilities_misc_utilities_aarch64.cmake
+++ utilities/misc_utilities/utilities_misc_utilities_aarch64.cmake
@@ -2,8 +2,11 @@
 include_guard(GLOBAL)
 message("utilities_misc_utilities_aarch64 component is included.")
 
+ADD_DEFINITIONS(-DSDK_USE_SYSCALL_STUB=1)
+
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/fsl_sbrk.c
+    ${CMAKE_CURRENT_LIST_DIR}/fsl_syscall_stub.c
 )
 
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
