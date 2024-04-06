0b2796dbfffb888825a2534a47e6d3c0466431dd
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2024-04-06 16:34:34 +0800

    devices: MIMX9596: add device system cmake
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

---
+++ devices/MIMX9596/device_system.cmake
@@ -0,0 +1,22 @@
+# Add set(CONFIG_USE_device_system true) in config.cmake to use this component
+
+include_guard(GLOBAL)
+message("${CMAKE_CURRENT_LIST_FILE} component is included.")
+
+if((CONFIG_DEVICE_ID STREQUAL MIMX9596xxxxN) AND (MCUX_DEVICE STREQUAL MIMX9596))
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/system_MIMX9596_cm7.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)
+
+include(device_CMSIS)
+
+else()
+
+message(SEND_ERROR "device_system dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
+
+endif()
