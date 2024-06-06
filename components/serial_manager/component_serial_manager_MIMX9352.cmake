8dbb7d8b289fef68abf7a01cc8a139a70f5b2abf
qizhang <jony.zhang@nxp.com>
2024-06-06 18:01:14 +0800

    Add driver for i.MX93
    Add pcal6254 driver.
    
    Modify serial_manager, enet driver.
    
    Signed-off-by: qizhang <jony.zhang@nxp.com>

---
+++ components/serial_manager/component_serial_manager_MIMX9352.cmake
@@ -0,0 +1,20 @@
+# Add set(CONFIG_USE_component_serial_manager true) in config.cmake to use this component
+
+include_guard(GLOBAL)
+message("${CMAKE_CURRENT_LIST_FILE} component is included.")
+
+if(CONFIG_USE_driver_common AND CONFIG_USE_component_lists AND (CONFIG_USE_component_serial_manager_uart OR CONFIG_USE_component_serial_manager_virtual OR CONFIG_USE_component_serial_manager_swo OR CONFIG_USE_component_serial_manager_spi))
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+  ${CMAKE_CURRENT_LIST_DIR}/fsl_component_serial_manager.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+  ${CMAKE_CURRENT_LIST_DIR}/.
+)
+
+else()
+
+message(SEND_ERROR "component_serial_manager.MIMX9352 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
+
+endif()
