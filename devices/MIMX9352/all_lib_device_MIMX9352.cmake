9b95299a516033789dfdd52d38b05ee92e2d1bf0
qizhang <jony.zhang@nxp.com>
2024-06-06 18:01:14 +0800

    Add driver for i.MX93
    Add pcal6254 driver.
    
    Modify serial_manager, enet driver.
    
    Signed-off-by: qizhang <jony.zhang@nxp.com>

--- devices/MIMX9352/all_lib_device_MIMX9352.cmake
+++ devices/MIMX9352/all_lib_device_MIMX9352.cmake
@@ -21,6 +21,7 @@ list(APPEND CMAKE_MODULE_PATH
     ${CMAKE_CURRENT_LIST_DIR}/../../drivers/rgpio
     ${CMAKE_CURRENT_LIST_DIR}/../../drivers/lpi2c
     ${CMAKE_CURRENT_LIST_DIR}/../../drivers/lpuart
+    ${CMAKE_CURRENT_LIST_DIR}/../../drivers/pcal6524
     ${CMAKE_CURRENT_LIST_DIR}/../../drivers/mu
     ${CMAKE_CURRENT_LIST_DIR}/../../drivers/pdm
     ${CMAKE_CURRENT_LIST_DIR}/../../drivers/sai
