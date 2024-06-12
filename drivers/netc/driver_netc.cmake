771ae20deba5edc20a4eeb7aa038d9eb6e3518cc
Yangbo Lu <yangbo.lu@nxp.com>
2024-06-12 13:38:58 +0900

    drivers: netc: add cmake files
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

---
+++ drivers/netc/driver_netc.cmake
@@ -0,0 +1,20 @@
+#Description: NETC Driver; user_visible: True
+include_guard(GLOBAL)
+message("driver_netc component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/fsl_netc_endpoint.c
+    ${CMAKE_CURRENT_LIST_DIR}/fsl_netc_ierb.c
+    ${CMAKE_CURRENT_LIST_DIR}/fsl_netc_mdio.c
+    ${CMAKE_CURRENT_LIST_DIR}/netc_hw/fsl_netc_hw.c
+    ${CMAKE_CURRENT_LIST_DIR}/netc_hw/fsl_netc_hw_port.c
+    ${CMAKE_CURRENT_LIST_DIR}/netc_hw/fsl_netc_hw_si.c
+    ${CMAKE_CURRENT_LIST_DIR}/netc_hw/fsl_netc_hw_enetc.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/.
+    ${CMAKE_CURRENT_LIST_DIR}/netc_hw/.
+)
+
+include(driver_common)
---
+++ drivers/netc/socs/imx95/driver_netc_soc_imx95.cmake
@@ -0,0 +1,13 @@
+#Description: NETC SOC Driver; user_visible: True
+include_guard(GLOBAL)
+message("driver_netc_soc_imx95 component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/fsl_netc_soc.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)
+
+include(driver_common)
