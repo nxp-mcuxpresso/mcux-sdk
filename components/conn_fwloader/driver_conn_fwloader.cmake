e143ebb1dcdefb00e85e45b7ff83214d5719faff
Fengming Ye <frank.ye@nxp.com>
2024-08-08 18:34:39 +0900

    components: conn_fwloader: update cmake file
    Update conn_fwloader cmake file for new changes.
    
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- components/conn_fwloader/driver_conn_fwloader.cmake
+++ components/conn_fwloader/driver_conn_fwloader.cmake
@@ -1,9 +1,18 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2023-24 NXP.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
 #Description: Driver conn_fwloader; user_visible: True
 include_guard(GLOBAL)
 message("driver_conn_fwloader component is included.")
 
+if(NOT CONFIG_BUILD_WITH_TFM)
+
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/fsl_loader.c
+    ${CMAKE_CURRENT_LIST_DIR}/fsl_loader_utils.c
     ${CMAKE_CURRENT_LIST_DIR}/nboot_hal.c
     ${CMAKE_CURRENT_LIST_DIR}/life_cycle.c
 )
@@ -11,6 +20,11 @@ target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
     ${CMAKE_CURRENT_LIST_DIR}/include
 )
+endif()
+
+if(CONFIG_BUILD_WITH_TFM)
+    add_compile_definitions(TFM_PARTITION_LOADER_SERVICE)
+endif()
 
 #OR Logic component
 if(CONFIG_USE_component_osa_free_rtos_RW612) 
