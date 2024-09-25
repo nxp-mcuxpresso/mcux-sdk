34def95430afcc8ff936ce2520af4915daa7da87
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2024-09-25 22:45:35 +0800

    drivers: mu1: add cmake and Kconfig
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

---
+++ drivers/mu1/driver_mu1.cmake
@@ -0,0 +1,14 @@
+#Description: MU1 Driver; user_visible: True
+include_guard(GLOBAL)
+message("driver_mu1 component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/fsl_mu.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)
+
+
+include(driver_common)
