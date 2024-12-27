0db10da15d9cc6222744d4854b2cb532b646dcd4
Aziz Sellami <aziz.sellami@nxp.com>
2024-12-27 13:56:39 +0100

    components: platform: imx95: add imx95lp4xevk15 on CMake files
    Signed-off-by: Aziz Sellami <aziz.sellami@nxp.com>

--- components/power/porting/platform/imx95/component_hal_power_imx95.cmake
+++ components/power/porting/platform/imx95/component_hal_power_imx95.cmake
@@ -3,7 +3,7 @@
 include_guard(GLOBAL)
 message("${CMAKE_CURRENT_LIST_FILE} component is included.")
 
-if((CONFIG_BOARD STREQUAL imx95lpd5evk19))
+if((CONFIG_BOARD STREQUAL imx95lp4xevk15) OR (CONFIG_BOARD STREQUAL imx95lpd5evk19))
 
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
   ${CMAKE_CURRENT_LIST_DIR}/.
--- components/sm/porting/platform/imx95/ca55/component_sm_imx95_ca55.cmake
+++ components/sm/porting/platform/imx95/ca55/component_sm_imx95_ca55.cmake
@@ -3,7 +3,7 @@
 include_guard(GLOBAL)
 message("${CMAKE_CURRENT_LIST_FILE} component is included.")
 
-if((CONFIG_BOARD STREQUAL imx95lpd5evk19) AND CONFIG_USE_driver_mu1 AND CONFIG_USE_component_sm_smt AND CONFIG_USE_component_sm_crc AND CONFIG_USE_component_sm_scmi)
+if(((CONFIG_BOARD STREQUAL imx95lp4xevk15) OR (CONFIG_BOARD STREQUAL imx95lpd5evk19)) AND CONFIG_USE_driver_mu1 AND CONFIG_USE_component_sm_smt AND CONFIG_USE_component_sm_crc AND CONFIG_USE_component_sm_scmi)
 
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
   ${CMAKE_CURRENT_LIST_DIR}/sm_platform.c
