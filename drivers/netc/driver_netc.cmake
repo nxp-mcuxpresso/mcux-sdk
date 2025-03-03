1277588e8c34439fc08c657d3176d8a354853643
Yangbo Lu <yangbo.lu@nxp.com>
2025-03-03 14:34:16 +0800

    drivers: netc: build switch driver per platform
    NETC switch driver should be built per platform. Now only
    i.MXRT1180 supports switch hardware.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- drivers/netc/driver_netc.cmake
+++ drivers/netc/driver_netc.cmake
@@ -2,6 +2,12 @@
 include_guard(GLOBAL)
 message("driver_netc component is included.")
 
+if(${MCUX_DEVICE} MATCHES "MIMX9596")
+    set(NETC_SWITCH_FILE "")
+else()
+    set(NETC_SWITCH_FILE "${CMAKE_CURRENT_LIST_DIR}/fsl_netc_switch.c")
+endif()
+
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/fsl_netc_endpoint.c
     ${CMAKE_CURRENT_LIST_DIR}/fsl_netc_timer.c
@@ -11,6 +17,7 @@ target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/netc_hw/fsl_netc_hw_port.c
     ${CMAKE_CURRENT_LIST_DIR}/netc_hw/fsl_netc_hw_si.c
     ${CMAKE_CURRENT_LIST_DIR}/netc_hw/fsl_netc_hw_enetc.c
+    ${NETC_SWITCH_FILE}
 )
 
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
